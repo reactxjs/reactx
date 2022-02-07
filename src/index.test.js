1134e93c413cd8361eaaeecee6e83b7bea90d35a
iamkaarp <joakim.stenberg88@gmail.com>
2022-02-07 10:59:51 +0100

    testing implemented

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,57 @@
-import { ExampleComponent } from '.'
+import ReactX from '.'
 
-describe('ExampleComponent', () => {
+const options = {
+  modules: [
+    {
+      name: 'test',
+      state: {
+        test: 'test'
+      },
+      getters: {
+        get: (state) => state.test
+      },
+      actions: {
+        test: ({ commit }, data) => {
+          commit('test', data)
+        }
+      },
+      mutations: {
+        test: (state, data) => {
+          state.test = data
+        }
+      }
+    }
+  ]
+}
+
+describe('ReactX', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(ReactX).toBeTruthy()
+  })
+
+  it('has options', () => {
+    const reactx = new ReactX(options)
+    expect(reactx.options).toEqual(options)
+  })
+
+  it('can get', () => {
+    const reactx = new ReactX(options)
+    const getter = reactx.getters('test/get')
+    expect(getter).toEqual('test')
+  })
+
+  it('can dispatch', () => {
+    const reactx = new ReactX(options)
+    reactx.dispatch('test/test', 'abcd')
+    const getter = reactx.getters('test/get')
+    expect(getter).toEqual('abcd')
+  })
+
+  it('can use local storage', () => {
+    options.modules[0].persistent = true
+    const reactx = new ReactX(options)
+    reactx.dispatch('test/test', 'abcd')
+    const getter = reactx.getters('test/get')
+    expect(getter).toEqual('abcd')
   })
 })
